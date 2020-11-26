960e0009b17f69d482da1ae44917b064b55140e9
Theodore Evans <theodore.evans@dai-labor.de>
2020-11-26 18:12:56 +0100

    added constructor for batchdata

--- PathPattern/Data/PatternBatchData.cs
+++ PathPattern/Data/PatternBatchData.cs
@@ -15,5 +15,26 @@
 
         public float ClusteringDistMean { get; }
         public float ClusteringDistStddev { get; }
+
+        public PatternBatchData(float imageSize,
+                                float radiusMeanDistMean,
+                                float radiusMeanDistStddev,
+                                float radiusStddevDistMean,
+                                float radiusStddevDistStddev,
+                                float nodeDensityDistMean,
+                                float nodeDensityDistStddev,
+                                float clusteringDistMean,
+                                float clusteringDistStddev)
+        {
+            ImageSize = imageSize;
+            RadiusMeanDistMean = radiusMeanDistMean;
+            RadiusMeanDistStddev = radiusMeanDistStddev;
+            RadiusStddevDistMean = radiusStddevDistMean;
+            RadiusStddevDistStddev = radiusStddevDistStddev;
+            NodeDensityDistMean = nodeDensityDistMean;
+            NodeDensityDistStddev = nodeDensityDistStddev;
+            ClusteringDistMean = clusteringDistMean;
+            ClusteringDistStddev = clusteringDistStddev;
+        }  
     }
 }
