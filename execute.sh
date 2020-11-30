97d9f67805a63092a96d4d1ed27122b000da57d7
Theodore Evans <theodore.evans@dai-labor.de>
2020-11-30 10:24:05 +0100

    added command line argument to execute.sh for num images

--- execute.sh
+++ execute.sh
@@ -7,8 +7,9 @@ else
 fi
 
 # The images folder should be created in the same directory
+# provide number of images to generate as argument to this script
 
 docker run -it --rm -v $(pwd)/images:/app/images \
     -v $(pwd)/$conf:/app/config.json \
     pathpattern \
-    dotnet PathPattern.dll batch config.json 10 images
+    dotnet PathPattern.dll batch config.json $1 images
